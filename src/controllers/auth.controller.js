30711ec419e2bc03854c81f42fc44bb6109734c5
RajdipChatterjee <rajdipchatterjee3000@gmail.com>
2026-05-20 12:04:10 +0530

    'loginUser' method created in 'auth.controller.js and added to auth.routes.js'

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -46,4 +46,42 @@ async function registerUser(req, res) {
     })
 }
 
-module.exports = {registerUser}
+async function loginUser(req, res) {
+    const {username, email, password} = req.body;
+
+    const user = await userModel.findOne({
+        $or: [
+            {username},
+            {email}
+        ]
+    })
+
+    if (!user) {
+        return res.status(401).json({message: "Invalid credentials"});
+    }
+
+    const isPasswordValid = bcrypt.compare(password, user.password);
+
+    if(!isPasswordValid){
+        return res.status(401).json({message: "Invalid credentials"})
+    }
+
+    const token = jwt.sign({
+        _id: user.id,
+        role: user.role
+    }, process.env.JWT_SECRET_KEY)
+
+    res.cookie("token", token);
+
+    return res.status(200).json({
+        message: "User logged in successfully",
+        user: {
+            id: user._id,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        }
+    })
+}
+
+module.exports = {registerUser, loginUser}
